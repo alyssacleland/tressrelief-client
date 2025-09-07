37214536dfbce607e4110173bc5704f91ff97e37
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-09-06 23:53:07 -0500

    added to the service form

--- src/components/forms/ServiceForm.js
+++ src/components/forms/ServiceForm.js
@@ -6,31 +6,87 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { getStylists } from '../../api/userData';
+import { useRouter } from 'next/navigation';
+import { InputGroup } from 'react-bootstrap';
+import { getServiceStylistOptions } from '../../api/userData';
 import { getCategories } from '../../api/categoriesData';
 
 const initialState = {
+  // id: '',
   name: '',
   description: '',
   duration: '',
   price: '',
   category: '',
-  stylistIds: [], // array for multi-select
+  image_url: '',
+  active: true,
+  stylist_ids: [], // array for multi-select
 };
 
-export default function ServiceForm({ serviceObj = {} }) {
+export default function ServiceForm({ serviceObj = initialState }) {
   const [currentService, setCurrentService] = useState(initialState);
-  const [stylists, setStylists] = useState([]);
-  const [categories, setCategories] = useState([]);
+  const [stylistOptions, setStylistOptions] = useState([]);
+  const [categoryOptions, setCategoryOptions] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
-    getCategories().then(setCategories);
-    getStylists().then(setStylists);
+    getCategories().then(setCategoryOptions);
   }, []);
 
-  // TODO: Prefill basic fields for edit
+  // if editing, seed state from serviceObj, otherwise start from initialState
+  useEffect(() => {
+    // EDIT
+    if (serviceObj?.id) {
+      setCurrentService(() => ({
+        id: serviceObj.id,
+        name: serviceObj.name,
+        description: serviceObj.description,
+        duration: serviceObj.duration,
+        price: serviceObj.price,
+        category: serviceObj.category,
+        image_url: serviceObj.image_url,
+        active: serviceObj.active,
+        stylist_ids: [],
+      }));
 
-  // TODO: Prefill stylistIds for edit
+      // get all stylists (including joined field, true or false depending on if they offer this service)
+      getServiceStylistOptions(serviceObj.id).then((options) => {
+        const allStylists = options;
+        // update stylist options in state
+        setStylistOptions(allStylists);
+        // isolate the stylists with joined=True so they can be added to stylist_ids on the service, and get their ids because stylist_ids expects an array of numbers, not objects
+        const preselected = allStylists.filter((stylist) => stylist.joined === true).map((stylist) => Number(stylist.id));
+        setCurrentService((prevState) => ({ ...prevState, stylist_ids: preselected }));
+      });
+      // CREATE
+    } else {
+      // load initial state and load stylistOptions state with joined=false
+      // API Note: getServiceStylistOptions without passing a service id: returns all stylists with joined property = false for each. I should really learn how to do the documentation hmm
+      setCurrentService(initialState);
+      getServiceStylistOptions().then(setStylistOptions);
+    }
+  }, [serviceObj?.id]);
+
+  const handleChange = (event) => {
+    // console.log(event)
+    const { name, value } = event.target;
+
+    if (name === 'price' || name === 'duration') {
+      const digits = String(value).replace(/[^\d]/g, ''); // strip non-digits
+      setCurrentService((prev) => ({ ...prev, [name]: digits }));
+      return;
+    }
+    setCurrentService((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  // TODO:
+  const handleStylistToggle = (event) => {};
+
+  // TODO:
+  const handleSubmit = (event) => {};
 
   return (
     <div>
@@ -38,7 +94,7 @@ export default function ServiceForm({ serviceObj = {} }) {
       <Form>
         <Form.Group className="mb-3">
           <Form.Label>Service Name</Form.Label>
-          <Form.Control required name="serviceName" value="" onChange="" placeholder="Enter Service Name" />
+          <Form.Control required name="name" value={currentService.name} onChange={handleChange} placeholder="Enter Service Name" />
           {/* <Form.Text className="text-muted">
           Well never share your email with anyone else.
         </Form.Text> */}
@@ -46,36 +102,39 @@ export default function ServiceForm({ serviceObj = {} }) {
 
         <Form.Group>
           <Form.Label>Category</Form.Label>
-          <Form.Select type="category" required value="" onChange="" placeholder="Select Category" name="category">
+          <Form.Select type="category" required value={currentService.category} onChange={handleChange} placeholder="Select Category" name="category">
             <option>Default select</option>
           </Form.Select>
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Stylists</Form.Label>
-          <Form.Select required value="" onChange="" placeholder="Enter Service Name" name="stylists">
+          <Form.Select required value="" onChange="" placeholder="Stylists (Check all that apply)" name="stylists">
             <option>Default select</option>
           </Form.Select>
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>Description</Form.Label>
-          <Form.Control required value="" onChange="" name="description" placeholder="Enter Description" />
+          <Form.Control required value={currentService.description} onChange={handleChange} name="description" placeholder="Enter Description" />
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>Duration (minutes)</Form.Label>
-          <Form.Control required value="" onChange="" type="number" placeholder="Enter Duration in Minutes" name="duration" />
+          <Form.Control required value={currentService.duration} onChange={handleChange} type="number" placeholder="Enter Duration in Minutes" name="duration" step="1" min="1" />
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>Price</Form.Label>
-          <Form.Control required value="" onChange="" type="number" placeholder="Enter Price" name="price" />
+          <InputGroup>
+            <InputGroup.Text>$</InputGroup.Text>
+            <Form.Control required value={currentService.price} onChange={handleChange} type="number" placeholder="Enter Price" name="price" min="0" step="1" />
+          </InputGroup>
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>Image URL</Form.Label>
-          <Form.Control required value="" onChange="" type="link" placeholder="Enter Image URL" name="imageurl" />
+          <Form.Control required value={currentService.image_url} onChange={handleChange} placeholder="Enter Image URL" name="image_url" />
         </Form.Group>
 
         <Button variant="primary" type="submit">
